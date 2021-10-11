78af624e42acc03d7492c3a2719d8a3ef522cd41
Crowdbotics <team@crowdbotics.com>
2021-10-11 10:35:07 +0000

    Initial commit.

---
+++ zenesys_31311/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for zenesys_31311 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zenesys_31311.settings')
+
+application = get_wsgi_application()
